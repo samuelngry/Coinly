eef4f419ed920e3ddaf8309ed32bddb1cf42edfc
samuelngry <samuelngry00@gmail.com>
2025-04-28 22:58:25 +0800

    chore: change backend port to 5000

--- backend/app.js
+++ backend/app.js
@@ -11,5 +11,5 @@ app.get('/', (req, res) => {
   res.send('Coinly Backend Running!');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
