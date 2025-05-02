e5a136301d884d0aa2e5e6e38ac0085542ad80cb
samuelngry <samuelngry00@gmail.com>
2025-05-02 16:48:48 +0800

    feat: connect Sequelize to PostgreSQL

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,20 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
+const authRoutes = require("./routes/authRoutes");
+const db = require("./config/db");
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Dummy route
-app.get('/', (req, res) => {
-  res.send('Coinly Backend Running!');
-});
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+db.authenticate().then(() => {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+})
+.catch((error) => {
+  console.log(error.message)
+});
