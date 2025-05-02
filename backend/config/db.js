9f218e491ee3e3c619aef036264859f94ffde690
samuelngry <samuelngry00@gmail.com>
2025-05-02 16:40:10 +0800

    chore: switch db config from pg Pool to Sequelize instance

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,8 @@
-const { Pool } = require('pg');
+const { Sequelize } = require('sequelize');
 
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    logging: false,
 });
 
-module.exports = pool;
+module.exports = sequelize;
