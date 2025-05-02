b111093a77cad2ecd4f649a0ddfb4344ae583109
samuelngry <samuelngry00@gmail.com>
2025-05-02 15:17:55 +0800

    feat: define User model with username, password and avatar_url fields

---
+++ backend/models/User.js
@@ -0,0 +1,25 @@
+const Sequelize = require('sequelize');
+const db = require("../config/db");
+
+const User = db.define('user', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    username: {
+        type: Sequelize.STRING(255),
+        unique: true,
+        allowNull: false,
+    },
+    password: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+    },
+    avatar_url: {
+        type: Sequelize.STRING(255),
+        allowNull: true,
+    },
+});
+
+module.exports = User;
