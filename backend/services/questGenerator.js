63108c4b940763cf413200ac9ab553620b512c63
samuelngry <samuelngry00@gmail.com>
2025-05-04 10:55:02 +0800

    feat: add user and preferences fetch in generateDynamicQuests

--- backend/services/questGenerator.js
+++ backend/services/questGenerator.js
@@ -2,3 +2,12 @@ const questComponents = require("../config/questComponents");
 const User = require("../models/User");
 const UserPreference = require("../models/UserPreference");
 const UserQuest = require("../models/UserQuest");
+
+async function generateDynamicQuests(userId) {
+    const user = await User.findByPk(userId);
+    const userPreference = await UserPreference.findByPk(userId);
+    
+    if (!user || !userPreference) {
+        throw new Error('User or user preferences not found');
+    }
+}
