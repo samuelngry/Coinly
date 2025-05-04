f79df6f769180cf7faa4ce79372c4ed49f9ce394
samuelngry <samuelngry00@gmail.com>
2025-05-04 17:04:03 +0800

    test: set up user registration and cleanup for user routes testing

---
+++ backend/tests/user.test.js
@@ -0,0 +1,33 @@
+const supertest = require('supertest');
+const { expect } = require('chai');
+const app = require('../server');
+const db = require('../config/db');
+const { User } = require("../models");
+
+describe('User Routes', () => {
+    let authToken;
+    let testUserId;
+
+    const testUser = {
+        username: 'testUser2',
+        password: '123456789',
+        avatar_url: 'http://example.com/avatar.png',
+    };
+
+    before(async () => {
+        await db.authenticate();
+        await User.destroy({ where: { username: testUser.username } });
+
+        const response = await supertest(app)
+            .post('/api/auth/register')
+            .send(testUser);
+
+        authToken = response.body.token;
+        testUserId = response.body.user.id;
+    });
+
+    after(async () => {
+        await User.destroy({ where: { id: testUserId } });
+        await db.close();
+    });
+})
