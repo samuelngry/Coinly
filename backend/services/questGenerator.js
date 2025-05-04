ae39482dfa3bc159d5aa169dc02b08898bf9cbd4
samuelngry <samuelngry00@gmail.com>
2025-05-04 10:51:52 +0800

    chore: set up questGenerator service with model and config imports

---
+++ backend/services/questGenerator.js
@@ -0,0 +1,4 @@
+const questComponents = require("../config/questComponents");
+const User = require("../models/User");
+const UserPreference = require("../models/UserPreference");
+const UserQuest = require("../models/UserQuest");
