82140491573bfb8495ec8d1b59c7f40a25e9b276
samuelngry <samuelngry00@gmail.com>
2025-05-12 22:48:38 +0800

    feat: add responsive mobile drawer to navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,17 @@
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
 import { navItems } from "../constants";
 import logo from "../assets/logo.png";
 
 const Navbar = () => {
+    const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false);
+
+    const toggleNavbar = () => {
+        setMobileDrawerOpen(!mobileDrawerOpen);
+    };
+
     return (
-        <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-800">
+        <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-100">
             <div className="container px-4 mx-auto relative text-sm">
                 <div className="flex justify-between items-center">
                     <div className="flex items-center flex-shrink-0">
@@ -25,7 +33,31 @@ const Navbar = () => {
                             Create Account
                         </a>
                     </div>
+                    <div className="lg:hidden md:flex flex-col justify-end">
+                        <button onClick={toggleNavbar}>
+                            {mobileDrawerOpen ? <X /> : <Menu />}
+                        </button>
+                    </div>
                 </div>
+                {mobileDrawerOpen && (
+                    <div className="fixed right-0 z-20 bg-neutral-100 w-full p-3 flex flex-col justify-center items-center lg:hidden">
+                        <ul>
+                            {navItems.map((item, index) => (
+                                <li key={index} className="py-4">
+                                    <a href={item.href}>{item.label}</a>
+                                </li>
+                            ))}
+                        </ul>
+                        <div className="flex space-x-6 p-3">
+                            <a href='#' className="py-2 px-3 border rounded-md">
+                                Sign In
+                            </a>
+                            <a href='#' className="bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md">
+                                Create Account
+                            </a>
+                        </div>
+                    </div>
+                )}
             </div>
         </nav>
     );
