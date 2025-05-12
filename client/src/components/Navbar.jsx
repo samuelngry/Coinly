a2ef25bf9792f76363317e62a9c58c668fdd1b80
samuelngry <samuelngry00@gmail.com>
2025-05-12 21:18:19 +0800

    fix: change rounded-mg typo to rounded-md for rounded medium border around create account

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
                         <a href="#" className="py-2 px-3 border rounded-md">
                             Sign In
                         </a>
-                        <a href="#" className="bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-mg">
+                        <a href="#" className="bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md">
                             Create Account
                         </a>
                     </div>
