9f5b419e14d6de6522e60e38d9c45591a9d5c138
samuelngry <samuelngry00@gmail.com>
2025-05-12 19:01:57 +0800

    feat: add CRUD routes for custom quests with authentication

--- backend/routes/customQuestRoutes.js
+++ backend/routes/customQuestRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const { getCustomQuests, addCustomQuest, updateCustomQuest, deleteCustomQuest } = require("../controllers/customQuestController");
 const verifyToken = require("../middleware/authMiddleware");
 
-router.post('/', verifyToken);
-router.get("/", verifyToken);
-router.put("/:id", verifyToken);
-router.delete("/:id", verifyToken);
+router.post('/', verifyToken, addCustomQuest);
+router.get("/", verifyToken, getCustomQuests);
+router.put("/:id", verifyToken, updateCustomQuest);
+router.delete("/:id", verifyToken, deleteCustomQuest);
 
 module.exports = router;
