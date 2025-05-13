ba72c7623d3dbf5459f83f60a85b0922b688968e
samuelngry <samuelngry00@gmail.com>
2025-05-13 21:23:33 +0800

    feat: add FeatureSection with section header and subheader

---
+++ client/src/components/FeatureSection.jsx
@@ -0,0 +1,11 @@
+const FeatureSection = () => {
+  return (
+    <div className="flex flex-col mt-6 lg:mt-40 items-center">
+      <h1 className="text-2xl sm:text-3xl lg:text-5xl tracking-wide text-center">So simple, it's fun.</h1>
+      <p className="text-sm sm:text-md lg:text-lg mt-3 text-neutral-400 text-center">It's just how you've always wanted saving to work.</p>
+      <div></div>
+    </div>
+  )
+}
+
+export default FeatureSection
