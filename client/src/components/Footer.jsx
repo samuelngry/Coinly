5b4d2e5e9238c0199e2bc448c0934096fdc8bb8b
samuelngry <samuelngry00@gmail.com>
2025-05-17 22:29:08 +0800

    feat: add rights license in footer with border above it

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className='bg-neutral-100 py-20'>
         <div className='container mx-auto text-sm relative'>
-            <div className='flex flex-col lg:flex-row justify-between items-center'>
+            <div className='flex flex-col lg:flex-row justify-between items-center border-b border-neutral-500 pb-10'>
                 <img className='w-15 h-15' src={logo} alt="logo" />
                 <ul className='flex space-x-6 mt-6'>
                     {resourcesLinks.map((item,index) => (
@@ -16,6 +16,11 @@ const Footer = () => {
                         </li>
                     ))}
                 </ul>
+            </div>
+            <div>
+                <p className='text-center mt-6'>
+                    &copy; {new Date().getFullYear()} Coinly. All rights reserved.
+                </p>
             </div>
         </div>
     </div>
