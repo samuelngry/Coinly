c12523362cc1c165d938c164b436a0ff5c127aba
samuelngry <samuelngry00@gmail.com>
2025-05-18 00:17:45 +0800

    feat: add image for right side of login page in desktop version

--- client/src/components/auth/LoginForm.jsx
+++ client/src/components/auth/LoginForm.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import loginImage from '../../assets/pets.jpeg'
 
 const LoginForm = () => {
   return (
     <div>
       <div className='flex flex-col lg:flex-row h-screen'>
-        <div className='flex flex-col justify-center items-center w-full lg:w-1/2'>
+        <div className='flex flex-col justify-center items-center w-full lg:w-1/2 px-4'>
           <div className='w-3/4 lg:w-2/3 max-w-md rounded-lg'>
-            <h1 className='text-3xl mb-6'>Login</h1>
+            <h1 className='text-3xl mb-6 text-center'>Log In</h1>
             <form>
               <div>
                 <label htmlFor='email' className='block mb-2'>
@@ -38,6 +39,10 @@ const LoginForm = () => {
               </p>
             </form>
           </div>
+        </div>
+        
+        <div className='hidden lg:flex w-full lg:w-1/2'>
+          <img className='w-full h-full object-cover object-center' src={loginImage} alt='Pets'></img>
         </div>
       </div>
     </div>
