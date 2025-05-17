c499fd17887a4e14dd28c8a383b30e9f8650ff59
samuelngry <samuelngry00@gmail.com>
2025-05-18 00:00:54 +0800

    feat: add left side of login form with email, password, log in button and sign up link

--- client/src/components/auth/LoginForm.jsx
+++ client/src/components/auth/LoginForm.jsx
@@ -3,8 +3,42 @@ import React from 'react'
 const LoginForm = () => {
   return (
     <div>
-      <div className='flex flex-col items-center'>
-        <h1>Login</h1>
+      <div className='flex flex-col lg:flex-row h-screen'>
+        <div className='flex flex-col justify-center items-center w-full lg:w-1/2'>
+          <div className='w-3/4 lg:w-2/3 max-w-md rounded-lg'>
+            <h1 className='text-3xl mb-6'>Login</h1>
+            <form>
+              <div>
+                <label htmlFor='email' className='block mb-2'>
+                  Email
+                </label>
+                <input
+                  className='border border-gray-300 rounded-md p-2 w-full' 
+                  id='email' 
+                  type='email' 
+                  placeholder='Enter your email'>
+                </input>
+              </div>
+              <div>
+                <label htmlFor='password' className='block mb-2 mt-4'>
+                  Password
+                </label>
+                <input
+                  className='border border-gray-300 rounded-md p-2 w-full'
+                  id='password'
+                  type='password'
+                  placeholder='Enter your password'>
+                </input>
+              </div>
+              <button className='bg-orange-500 text-white hover:bg-orange-800 rounded-md p-2 mt-4 w-full'>
+                Log in
+              </button>
+              <p className='mt-4 text-center'>
+                Don't have an account? <a href='/signup' className='underline text-orange-500 hover:text-orange-800'>Sign up</a>
+              </p>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   )
