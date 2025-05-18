2cbd37bd023fd73423e20b2e30fc6244070b4b3c
samuelngry <samuelngry00@gmail.com>
2025-05-18 18:58:13 +0800

    feat: add signup page, signup form, and rote in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/pages/LandingPage";
 import LoginPage from "./components/pages/LoginPage";
+import SignupPage from "./components/pages/SignupPage";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<SignupPage />} />
       </Routes>
     </Router>
   );
---
+++ client/src/components/pages/SignupPage.jsx
@@ -0,0 +1,11 @@
+import SignupForm from "../auth/SignupForm"
+
+const SignupPage = () => {
+  return (
+    <div style={{ backgroundColor: 'var(--old-lace)' }}>
+      <SignupForm />
+    </div>
+  )
+}
+
+export default SignupPage
