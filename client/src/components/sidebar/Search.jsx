ad7ea1efebce4b835888da9c681c90c7cc625ef9
samuelngry <samuelngry00@gmail.com>
2025-05-19 21:07:08 +0800

    feat: allow users to use shortcut command + K to search

--- client/src/components/sidebar/Search.jsx
+++ client/src/components/sidebar/Search.jsx
@@ -1,23 +1,29 @@
 import { FiCommand, FiSearch } from 'react-icons/fi';
+import React from 'react'
+import CommandMenu from './CommandMenu';
 
 const Search = () => {
-  return (
-    <>
-        <div className="bg-stone-100 mb-4 relative rounded flex items-center px-2 py-1.5 text-sm">
-            <FiSearch className='mr-2' />
-            <input 
-                type='text'
-                placeholder='Search'
-                className='w-full pr-8 bg-transparent placeholder:text-stone-300 focus:outline-none'
-            />
+    const [open, setOpen] = React.useState(false);
 
-            <span className='absolute right-2 flex items-center text-xs gap-0.5 p-0.5 bg-white rounded'>
-                <FiCommand />F
-            </span>
-        </div>
-        
-    </>  
-  )
+    return (
+        <>
+            <div className="bg-stone-100 mb-4 relative rounded flex items-center px-2 py-1.5 text-sm">
+                <FiSearch className='mr-2' />
+                <input 
+                    type='text'
+                    placeholder='Search'
+                    className='w-full pr-8 bg-transparent placeholder:text-stone-300 focus:outline-none'
+                />
+
+                <span className='absolute right-2 flex items-center text-xs gap-0.5 p-0.5 bg-white rounded'>
+                    <FiCommand />K
+                </span>
+            </div>
+
+            <CommandMenu open={open} setOpen={setOpen} />
+            
+        </>  
+    )
 }
 
 export default Search
