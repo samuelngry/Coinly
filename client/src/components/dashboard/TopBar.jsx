b2f64687279b1ad00991c50b135b0d0246dde358
samuelngry <samuelngry00@gmail.com>
2025-05-21 23:03:04 +0800

    feat: add calendar button to TopBar with icon and label

--- client/src/components/dashboard/TopBar.jsx
+++ client/src/components/dashboard/TopBar.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { FiCalendar } from 'react-icons/fi'
 
 const TopBar = () => {
   return (
     <div className='border-b mb-4 pb-4 mt-2 border-stone-300 px-4'>
-        <div className='p-0.5'>
-            <span className='text-sm font-semibold block'>Hey, Yi Jing!</span>
-            <span className='text-neutral-500 block text-xs'>Wednesday, May 21st 2025</span>
+        <div className='flex items-center justify-between p-0.5'>
+            <div>
+                <span className='text-sm font-semibold block'>Hey, Yi Jing!</span>
+                <span className='text-neutral-500 block text-xs'>Wednesday, May 21st 2025</span>
+            </div>
+            <button className='flex text-sm items-center gap-1.5 bg-orange-500 hover:bg-orange-800 text-white rounded-md px-2 py-1'>
+                <FiCalendar className='text-white' />
+                <span>Prev 6 Months</span>
+            </button>
         </div>
     </div>
   )
