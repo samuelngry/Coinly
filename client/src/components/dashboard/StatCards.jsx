6cb5c40777ea0b703e80d462f58298faafbda16b
samuelngry <samuelngry00@gmail.com>
2025-05-22 21:59:40 +0800

    feat: add daily goal card next to pet mood in dashboard

--- client/src/components/dashboard/StatCards.jsx
+++ client/src/components/dashboard/StatCards.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import { FiZap, FiSmile, FiTrendingDown, FiTrendingUp } from 'react-icons/fi'
+import { FiZap, FiSmile, FiTarget, FiTrendingDown, FiTrendingUp } from 'react-icons/fi'
 import { FaFire } from 'react-icons/fa'
 
-// TODO: Create card components that displays the stats of the user
 const StatCards = () => {
   return (
     <>
@@ -30,11 +29,17 @@ const StatCards = () => {
         period='yesterday'
         icon={<FiSmile className='w-10 h-10 bg-green-100 text-green-400 rounded-full px-2 py-2 shrink-0'/>}
       />
+      <Card 
+        title='Daily Goal'
+        value='40%'
+        trend='down'
+        percent='2.3%'
+        period='past week'
+        icon={<FiTarget className='w-10 h-10 bg-blue-100 text-blue-400 rounded-full px-2 py-2 shrink-0' />}
+      />
     </>
   );
 };
-
-// TODO: Create layout for the card
 
 const Card = ({
     title= '',
