303515845f67979409e1fe6b628425055415e8ab
samuelngry <samuelngry00@gmail.com>
2025-05-23 21:22:47 +0800

    feat: add pet mood images to petWidget component

--- client/src/components/dashboard/Grid.jsx
+++ client/src/components/dashboard/Grid.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import StatCards from './StatCards'
+import PetWidget from './PetWidget'
 
 const Grid = () => {
   return (
     <div className='px-4 grid gap-6 grid-cols-12'>
       <StatCards />
+      <PetWidget />
     </div>
   )
 }
---
+++ client/src/components/dashboard/PetWidget.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import happyPetImage from "../../assets/pet-happy.png"
+import sadPetImage from "../../assets/pet-sad.png"
+import excitedPetImage from "../../assets/pet-excited.png"
+import neutralPetImage from "../../assets/pet-neutral.png"
+import angryPetImage from "../../pet-angry.png"
+
+const PetWidget = () => {
+    const petMoodImages = {
+        happy: happyPetImage,
+        sad: sadPetImage,
+        excited: excitedPetImage,
+        neutral: neutralPetImage,
+        angry: angryPetImage,
+    };
+
+    return (
+        <div>
+        
+        </div>
+    )
+}
+
+export default PetWidget
