b5b03f9c3281be468eda20a49773a09bb22eefbd
samuelngry <samuelngry00@gmail.com>
2025-05-28 13:42:01 +0800

    feat: replace bar chart to line chart to show Weekly xp Progress in ActivityGraph

--- client/src/components/dashboard/ActivityGraph.jsx
+++ client/src/components/dashboard/ActivityGraph.jsx
@@ -1,54 +1,41 @@
-import React from 'react'
-import {
-  BarChart,
-  Bar,
-  XAxis,
-  YAxis,
-  Tooltip,
-  CartesianGrid,
-  ResponsiveContainer,
-  ReferenceLine,
-  Cell
-} from 'recharts';
+import React, { PureComponent } from 'react'
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const data = [
-  { name: 'Mon', xp: 120 },
-  { name: 'Tue', xp: 98 },
-  { name: 'Wed', xp: 150 },
-  { name: 'Thu', xp: 200 },
-  { name: 'Fri', xp: 170 },
-  { name: 'Sat', xp: 90 },
-  { name: 'Sun', xp: 130 },
+  { name: 'M', xp: 120 },
+  { name: 'T', xp: 98 },
+  { name: 'W', xp: 150 },
+  { name: 'T', xp: 200 },
+  { name: 'F', xp: 170 },
+  { name: 'S', xp: 90 },
+  { name: 'S', xp: 130 },
 ];
 
 const ActivityGraph = () => {
   return (
     <div className='p-4 col-span-12 lg:col-span-6 rounded-2xl bg-white shadow'>
         <div className='flex items-start justify-between'>
-            <h3>XP Progress</h3>
+            <h3>Weekly Progress</h3>
         </div>
         <div className="w-full h-64 mt-2">
             <ResponsiveContainer width="100%" height="100%">
-                <BarChart data={data} margin={{ top: 16, right: 24, left: 0, bottom: 0 }}>
-                    <CartesianGrid strokeDasharray="3 3" stroke="#e2e8f0" />
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                    <Tooltip cursor={{ fill: '#fef9c3' }} />
-                    <Bar
-                        dataKey='xp'
-                        radius={[12, 12, 0, 0]}
-                        isAnimationActive={true}
-                        animationDuration={800}
-                        animationEasing='ease-in-out'
-                    >
-                        {data.map((entry, index) => (
-                            <Cell
-                                key={`cell-${index}`}
-                                fill={entry.name === 'Sun' ? '#f97316' : '#facc15'}
-                            />
-                        ))}
-                    </Bar>                
-                </BarChart>
+                <LineChart
+                width={500}
+                height={300}
+                data={data}
+                margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                }}
+            >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Line type="linear" dataKey="xp" stroke="#1CB0F6" dot={{ r: 4, fill: '#1CB0F6', stroke: '#1CB0F6' }} activeDot={{ r: 6, fill: '#1CB0F6', stroke: '#1CB0F6' }} />
+                </LineChart>
             </ResponsiveContainer>
         </div>
     </div>
