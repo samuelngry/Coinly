4ced2662e6e290b793a214230a17e13975894c2c
samuelngry <samuelngry00@gmail.com>
2025-05-30 17:30:25 +0800

    feat: add step to show progress bar depending on step in onboarding

--- client/src/components/pages/OnboardingPage.jsx
+++ client/src/components/pages/OnboardingPage.jsx
@@ -1,9 +1,11 @@
 import OnboardingHeader from "../onboarding/OnboardingHeader";
 
 const OnboardingPage = () => {
+    const [step, setStep] = useState(1);
+
     return (
         <div className="max-w-7xl mx-auto py-10" >
-            <OnboardingHeader />
+            <OnboardingHeader step={step} />
         </div>
     );
 };
