074bdb4566eccc142ef8aacea7159b8ded369d32
samuelngry <samuelngry00@gmail.com>
2025-06-01 12:16:09 +0800

    feat: add button for user to select food category in first step

--- client/src/components/onboarding/OnboardingStep.jsx
+++ client/src/components/onboarding/OnboardingStep.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
 
 const OnboardingStep = ({ step, setStep, answers, setAnswers }) => {
+    const [selectedCategories, setSelectedCategories] = useState([]);
+
     if (step === 1) {
     return (
         <>
             <p>What's your biggest spending category?</p>
+            <div>
+                <button onClick={() => {
+                    if (selectedCategories.includes("Food")) {
+                        setSelectedCategories(selectedCategories.filter((c) => c != "Food"));
+                    } else {
+                        setSelectedCategories([...selectedCategories, "Food"]);
+                    };
+                }}>
+                    Food
+                </button>
+            </div>
 
         </>
-    )
-}
+        )
+    };
+};
 
 export default OnboardingStep
