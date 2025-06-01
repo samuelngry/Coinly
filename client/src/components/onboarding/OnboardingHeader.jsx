abbc7783ae07a0b737fd06ca39041cdbca4b278e
samuelngry <samuelngry00@gmail.com>
2025-06-01 23:02:25 +0800

    feat: hide back button if user is at step 0

--- client/src/components/onboarding/OnboardingHeader.jsx
+++ client/src/components/onboarding/OnboardingHeader.jsx
@@ -3,12 +3,18 @@ import { ArrowLeft } from 'lucide-react'
 
 const OnboardingHeader = ({ step, setStep }) => {
     const progress = step * 25;
+
     return (
         <div className='flex items-center w-full rounded-full px-5'>
             <button
                 onClick={() => {
                     setStep(prevStep => prevStep - 1);
                 }}
+                className={`${
+                    step === 0
+                    ? 'hidden'
+                    : 'visible'
+                }`}
             >
                 <ArrowLeft className='h-5 w-5 mr-2 text-gray-400 bg-white hover:text-gray-800' alt='back' />
             </button>
