d40162eb794bf8296e8e3dcfd24b4e2aac7e95bd
samuelngry <samuelngry00@gmail.com>
2025-06-01 15:01:51 +0800

    feat: add continue button for users to continue to next step once categories selected and only appear when at least one category is selected

--- client/src/components/onboarding/OnboardingStep.jsx
+++ client/src/components/onboarding/OnboardingStep.jsx
@@ -22,27 +22,41 @@ const OnboardingStep = ({ step, setStep, answers, setAnswers }) => {
     return (
         <div className='flex flex-col items-center justify-center mt-30'>
             <h1 className='mb-4 font-semibold text-lg'>What's your biggest spending category?</h1>
-            <div className='grid grid-cols-3 gap-4'>
-                {categories.map(({ label, image }) => (
-                    <button
-                        key={label}
-                        onClick={() => {
-                            if (selectedCategories.includes(label)) {
-                                setSelectedCategories(selectedCategories.filter((c) => c != label));
-                            } else if (!selectedCategories.includes(label) && selectedCategories.length < 2) {
-                                setSelectedCategories([...selectedCategories, label]);
-                            }
-                        }}
-                        className={`rounded-lg p-4 border-2 hover:bg-orange-50 hover:text-orange-400 ${
-                            selectedCategories.includes(label)
-                                ? "bg-orange-100 border-orange-500 text-orange-500"
-                                : "border-gray-300"
-                        }`}
-                    >
-                        <img src={image} alt={label} className='h-20 w-20 mx-auto mb-2' />
-                        <span className='text-sm font-medium'>{label}</span>
-                    </button>
-                ))}
+            <div>
+                <div className='grid grid-cols-3 gap-4 mb-4'>
+                    {categories.map(({ label, image }) => (
+                        <button
+                            key={label}
+                            onClick={() => {
+                                if (selectedCategories.includes(label)) {
+                                    setSelectedCategories(selectedCategories.filter((c) => c != label));
+                                } else if (!selectedCategories.includes(label) && selectedCategories.length < 2) {
+                                    setSelectedCategories([...selectedCategories, label]);
+                                }
+                            }}
+                            className={`rounded-lg p-4 border-2 hover:bg-orange-50 hover:text-orange-400 ${
+                                selectedCategories.includes(label)
+                                    ? "bg-orange-100 border-orange-500 text-orange-500"
+                                    : "border-gray-300"
+                            }`}
+                        >
+                            <img src={image} alt={label} className='h-20 w-20 mx-auto mb-2' />
+                            <span className='text-sm font-medium'>{label}</span>
+                        </button>
+                    ))}
+                </div>
+                <button 
+                    className={`w-full border rounded-xl py-3 shadow text-xs bg-orange-500 text-white ${
+                        selectedCategories.length > 0
+                            ? "visible"
+                            : "hidden"
+                    }`}
+                    onClick={() => {
+                        setStep(2);
+                    }}
+                >
+                    CONTINUE
+                </button>
             </div>
         </div>
         )
