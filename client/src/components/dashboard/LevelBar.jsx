fea3b4d82917891ec4427e3576e32f0ed09afd71
samuelngry <samuelngry00@gmail.com>
2025-06-08 18:30:08 +0800

    feat: add xp bar under level in MainPage

--- client/src/components/dashboard/LevelBar.jsx
+++ client/src/components/dashboard/LevelBar.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import { ProgressBar } from 'primereact/progressbar';
 
 const LevelBar = () => {
   return (
     <div className='flex flex-col'>
-      <div className='flex justify-between'>
-        <h1>LV 26</h1>
-        <span>1138/1500 XP</span>
+      <div className='flex items-center justify-between mb-2'>
+        <h1 className='text-md'>LV 26</h1>
+        <span className='text-xs'>1138/1500 XP</span>
       </div>
+      <ProgressBar className="custom-xp-bar" value={75} showValue={ false } style={{ height: '0.6rem' }}></ProgressBar>
     </div>
   )
 }
