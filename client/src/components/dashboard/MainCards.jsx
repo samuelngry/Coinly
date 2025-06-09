ee2186bb84cf699c93e68989c18617e0cea110fd
samuelngry <samuelngry00@gmail.com>
2025-06-09 17:14:56 +0800

    feat: add shadow for mainCards to make it stand out more

--- client/src/components/dashboard/MainCards.jsx
+++ client/src/components/dashboard/MainCards.jsx
@@ -5,7 +5,7 @@ import fireIcon from '../../assets/fire.png'
 const MainCards = () => {
   return (
     <div className='grid grid-cols-2 mt-4 gap-4 max-w-lg mx-auto'>
-      <div className='border border-neutral-300 shadow rounded-xl'>
+      <div className='border border-neutral-300 shadow-lg shadow-black/10 rounded-xl'>
         <div className='flex items-center p-2 gap-2'>
             <img src={fireIcon} alt='Streak' className='w-7 h-7'/>
             <div className='flex flex-col'>
@@ -14,7 +14,7 @@ const MainCards = () => {
             </div>
         </div>
       </div>
-      <div className='border border-neutral-300 shadow rounded-xl'>
+      <div className='border border-neutral-300 shadow-lg shadow-black/10 rounded-xl'>
         <div className='flex items-center p-2 gap-2'>
             <img src={questIcon} alt='Quest' className='w-7 h-7'/>
             <div className='flex flex-col'>
