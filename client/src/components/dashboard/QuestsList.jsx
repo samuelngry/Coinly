c1189513b7b26afd9b9b79d82cc51ecccbf8cf72
samuelngry <samuelngry00@gmail.com>
2025-06-09 15:03:34 +0800

    feat: make done button for quest stand out more and make the card responsive

--- client/src/components/dashboard/QuestsList.jsx
+++ client/src/components/dashboard/QuestsList.jsx
@@ -1,25 +1,30 @@
 import React from 'react'
 import dailyIcon from '../../assets/daily.png'
 import pawIcon from '../../assets/paw.png'
-import tickIcon from '../../assets/tick.png'
+import { CheckIcon } from 'lucide-react'
 
 const QuestsList = () => {
   return (
     <div className='flex flex-col mt-6'>
       <h3 className='text-sm'>Start your day!</h3>
       <div className='space-y-2'>
-        <div className='mt-3 p-2 rounded-xl shadow border border-neutral-300 flex items-center gap-2'>
-            <img src={dailyIcon} alt='Quest' className='w-10 h-10'/>
-            <span className='text-xs max-w-40'>Track all your expenses today</span>
-            <div className='flex items-center'>
-                <span className='text-xs pl-2'>20</span>
-                <img src={pawIcon} alt='Paw' className='w-3 h-3 ml-1'/>
+        <div className='mt-3 p-3 rounded-xl shadow border border-neutral-300 flex justify-between items-center gap-2'>
+            <div className='flex items-center gap-2 flex-1'>
+                <img src={dailyIcon} alt='Quest' className='w-10 h-10'/>
+                <span className='text-xs break-words'>Track all your expenses today</span>
+            </div>
+            <div className='flex items-center shrink-0'>
+                <div className="bg-orange-100 text-orange-600 px-1.5 py-1 rounded-full text-xs font-semibold flex items-center gap-1">
+                    <span>20</span>
+                    <img src={pawIcon} className="w-3 h-3" />
+                </div>
                 <button
                 >
-                    <img src={tickIcon} alt='Tick' className='w-7 h-7 ml-2'/>
+                    <div className='w-8 h-8 rounded-xl border border-neutral-300 shadow-xl flex items-center justify-center hover:bg-green-100 ml-1.5'>
+                        <CheckIcon className='w-5 h-5 text-green-500'/>
+                    </div>
                 </button>
             </div>
-            
         </div>
       </div>
     </div>
