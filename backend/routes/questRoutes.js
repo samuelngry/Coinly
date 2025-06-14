57754d0dcb6f6088689f1f0edfaf26b6115ef888
samuelngry <samuelngry00@gmail.com>
2025-06-14 15:31:11 +0800

    refactor: remove acceptQuests route from questRoutes and update generateQuest route

--- backend/routes/questRoutes.js
+++ backend/routes/questRoutes.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { generateQuests, acceptQuests, completeQuests } = require("../controllers/questController");
+const { generateQuests, completeQuests } = require("../controllers/questController");
 const verifyToken = require("../middleware/authMiddleware");
 
-router.post("/generate", verifyToken, generateQuests);
-router.post("/:id/accept", verifyToken, acceptQuests);
+router.post("/", verifyToken, generateQuests);
 router.post("/:id/complete", verifyToken, completeQuests);
 
 module.exports = router;
