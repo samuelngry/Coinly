3c16fe98c77d4147e1e7bba831816f87c35d326b
samuelngry <samuelngry00@gmail.com>
2025-06-15 15:06:05 +0800

    feat: update LevelBar component to show dynamic xp and level progress

--- client/src/components/dashboard/LevelBar.jsx
+++ client/src/components/dashboard/LevelBar.jsx
@@ -2,18 +2,21 @@ import React from 'react'
 import { ProgressBar } from 'primereact/progressbar';
 import { PawPrint } from 'lucide-react';
 
-const LevelBar = () => {
+const LevelBar = ({xp, level}) => {
+  const currentLevelUpXp = 100 + (level - 1) * 50;
+  const progress = Math.min((xp / currentLevelUpXp) * 100, 100);
+
   return (
     <div className='flex flex-col'>
       <div className='flex items-center justify-between mb-2'>
-        <h1 className='text-md'>LV 26</h1>
+        <h1 className='text-md'>{level}</h1>
         <div className='flex items-end gap-1.5'>
-          <span className='text-xs leading-none'>1138/1500</span>
+          <span className='text-xs leading-none'>{xp}</span>
           <PawPrint alt='Paw' className='w-4 h-4 text-orange-500'/>
         </div>
         
       </div>
-      <ProgressBar className="custom-xp-bar" value={75} showValue={ false } style={{ height: '0.6rem' }}></ProgressBar>
+      <ProgressBar className="custom-xp-bar" value={ progress } showValue={ false } style={{ height: '0.6rem' }}></ProgressBar>
     </div>
   )
 }
