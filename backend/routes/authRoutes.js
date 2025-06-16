6bfec6b5b1b22b7ea5ef6e4ff571edcd10133daf
samuelngry <samuelngry00@gmail.com>
2025-06-16 20:33:05 +0800

    fix: remove avatar upload in authRoutes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const { register, login } = require("../controllers/authController");
-const { upload } = require("../middleware/multerMiddleware");
 
-router.post("/register", upload.single('avatar'), register);
-router.post("/login", upload.single('avatar'), login);
+router.post("/register", register);
+router.post("/login", login);
 
 module.exports = router;
