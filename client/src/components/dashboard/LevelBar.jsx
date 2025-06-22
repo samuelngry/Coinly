19d775420ae93ab64ac22f9482a6faf4f5186766
samuelngry <samuelngry00@gmail.com>
2025-06-22 10:04:11 +0800

    refactor: add 'LV' before the level number so that it is more clear to users

--- client/src/components/dashboard/LevelBar.jsx
+++ client/src/components/dashboard/LevelBar.jsx
@@ -9,7 +9,7 @@ const LevelBar = ({xp, level}) => {
   return (
     <div className='flex flex-col'>
       <div className='flex items-center justify-between mb-2'>
-        <h1 className='text-md'>{level}</h1>
+        <h1 className='text-md'>LV {level}</h1>
         <div className='flex items-end gap-1.5'>
           <span className='text-xs leading-none'>{xp}</span>
           <PawPrint alt='Paw' className='w-4 h-4 text-orange-500'/>
