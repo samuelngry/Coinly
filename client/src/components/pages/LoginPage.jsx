899da1c0b79093207896674ff4b2ecdde9ff99f1
samuelngry <samuelngry00@gmail.com>
2025-06-22 21:19:51 +0800

    feat: navigate user to dashboard if logged in and onboarding completed and onboard if logged in and onboarding incomplete

--- client/src/components/pages/LoginPage.jsx
+++ client/src/components/pages/LoginPage.jsx
@@ -1,6 +1,25 @@
 import LoginForm from "../auth/LoginForm";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const LoginPage = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        const user = localStorage.getItem('user');
+
+        if (token && user) {
+            const parsedUser = JSON.parse(user);
+
+            if (parsedUser.onboarding_completed) {
+                navigate('/dashboard');
+            } else {
+                navigate('/onboard');
+            }
+        } 
+    }, [navigate])
+    
     return (
         <div style={{ backgroundColor: 'var(--old-lace)' }}>
             <LoginForm />
