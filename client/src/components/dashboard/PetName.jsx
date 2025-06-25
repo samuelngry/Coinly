3a1ccb030e1fae45030cbf9efda4cf0d3c9e858b
samuelngry <samuelngry00@gmail.com>
2025-06-26 00:45:40 +0800

    feat: make edit pet name open dialog instead of on the dashboard itself

--- client/src/components/dashboard/PetName.jsx
+++ client/src/components/dashboard/PetName.jsx
@@ -1,11 +1,24 @@
 import React, { useState } from 'react'
 import EditIcon from '../../assets/edit.png'
+import {
+  Button,
+  Dialog,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+  Input,
+  Checkbox,
+  DialogBackdrop,
+} from "@material-tailwind/react";
 
 const PetName = ({ name, onComplete }) => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [open, setOpen] = useState(false);
   const [newName, setNewName] = useState(name);
 
-  const openForm = () => setIsOpen(true);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () =>setOpen(false);
 
   const handleNameChange = (e) => {
     setNewName(e.target.value);
@@ -14,44 +27,45 @@ const PetName = ({ name, onComplete }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     onComplete(newName);
-    setIsOpen(false);
+    setOpen(false);
   }
 
   const handleCancel = () => {
     setNewName(name);
-    setIsOpen(false);
+    setOpen(false);
   }
 
   return (
     <div className='px-3 py-1 flex items-center justify-center gap-2'>
-      {!isOpen ? (
-        <>
-          <h2 className='lg:text-lg'>{name}</h2>
-          <button onClick={openForm} >
-            <img src={EditIcon} alt='Edit' className='w-3.5 h-3.5'/>
-          </button>
-        </>
-      ) : (
-        <form onSubmit={handleSubmit} className='flex items-center gap-2'>
-          <input
-            type='text'
-            value={newName}
-            onChange={handleNameChange}
-            className='border px-2 py-1 rounded'
-            autoFocus
-          />
-          <button type='submit' className='text-orange-500'>
-            Save
-          </button>
-          <button
-            type='button'
-            onClick={handleCancel}
-            className='text-gray-500'
-          >
-            Close
-          </button>
-        </form>
-      )}
+        <h2 className='lg:text-lg'>{name}</h2>
+        <button onClick={handleOpen} >
+          <img src={EditIcon} alt='Edit' className='w-3.5 h-3.5'/>
+        </button>
+
+        {/* Dialog for editing pet name */}
+        <Dialog 
+          size='xs'
+          open={open}
+          onClose={handleCancel}
+          handler={handleOpen}
+          className='flex justify-center items-center'>
+        <Card className='mx-auto w-full max-w-[24rem]'>
+          <CardHeader className='text-center'>
+            <Typography className='text-sm font-bold p-2'>
+              Edit Pet Name
+            </Typography>
+          </CardHeader>
+          <Input size='lg' value={newName} onChange={handleNameChange} className='border border-gray-300 rounded-lg' autoFocus />
+          <CardFooter className='flex justify-between gap-2 p-4'>
+            <Button variant='gradient' onClick={handleSubmit} className='bg-orange-500 hover:bg-orange-800 text-white' fullWidth>
+              Confirm
+            </Button>
+            <Button variant="text" onClick={handleCancel} fullWidth className="mt-2">
+                Cancel
+            </Button>
+          </CardFooter>
+        </Card>
+      </Dialog>
     </div>
   )
 }
