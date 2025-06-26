c4b1f6c1384aed50235f3bd9d93a743ad58a6058
samuelngry <samuelngry00@gmail.com>
2025-06-26 22:46:33 +0800

    feat: add route to fetch user data

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const verifyToken = require("../middleware/authMiddleware");
-const { updateUsername, updateAvatar, savePreferences } = require("../controllers/userController");
+const { updateUsername, updateAvatar, savePreferences, getUserData } = require("../controllers/userController");
 
+router.get("/", verifyToken, getUserData);
 router.put("/update-username", verifyToken, updateUsername);
 router.put("/update-avatar", verifyToken, updateAvatar);
 router.post("/preferences", verifyToken, savePreferences);
