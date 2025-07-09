69cdf6b5a341e8dc321c1f776b7d3a7f135a47e5
samuelngry <samuelngry00@gmail.com>
2025-07-09 23:58:24 +0800

    feat: align total xp and line chart

--- client/src/pet/PetStat.jsx
+++ client/src/pet/PetStat.jsx
@@ -4,7 +4,6 @@ import {
   Line,
   XAxis,
   YAxis,
-  Tooltip,
   CartesianGrid,
   ResponsiveContainer,
 } from 'recharts';
@@ -15,27 +14,32 @@ const PetStat = ({ data }) => {
     <div className='mt-6 w-full max-w-2xl mx-auto'>
       <h2 className='mb-2'>Weekly Progress</h2>
       <div className='border border-neutral-300 shadow-lg shadow-black/10 rounded-xl'>
-        <div className='flex justify-between mx-3 my-2'>
-            <span className='text-orange-500 font-semibold text-sm'>Total</span>
+        <div className='flex justify-between mx-6.5 my-2'>
             <div className='flex items-center gap-1'>
-                <span className='text-orange-500 font-semibold text-sm'>59</span>
+                <div className='w-2 h-2 rounded-full bg-orange-500'></div>
+                <span className='text-orange-500 font-semibold text-sm'>Total</span>
+            </div>
+            <div className='flex items-center gap-1'>
+                <span className='text-orange-500 font-semibold text-md'>59</span>
                 <PawPrint alt='Paw' className='w-4 h-4 text-orange-500'/>
             </div>
         </div>
-        <ResponsiveContainer width="100%" height={200}>
-            <LineChart data={data}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="day" />
-            <YAxis allowDecimals={false} />
-            <Line
-                type="linear"
-                dataKey="xp"
-                stroke="#F97316"
-                strokeWidth={2}
-                dot={{ r: 4, fill: '#f97316' }}
-            />
-            </LineChart>
-        </ResponsiveContainer>
+        <div className='flex justify-center items-center mt-3'>
+            <ResponsiveContainer width="100%" height={200}>
+                <LineChart data={data} margin={{ right: 30}}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="day" />
+                <YAxis allowDecimals={false} />
+                <Line
+                    type="linear"
+                    dataKey="xp"
+                    stroke="#f97316"
+                    strokeWidth={2}
+                    dot={{ r: 4, fill: '#f97316' }}
+                />
+                </LineChart>
+            </ResponsiveContainer>
+        </div>
       </div>
     </div>
   );
