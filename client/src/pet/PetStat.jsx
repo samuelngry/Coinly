3b9692929b1fcfed5c0b558d74d84c074bf7358a
samuelngry <samuelngry00@gmail.com>
2025-07-09 23:23:51 +0800

    feat: update XP to paw for user understanding

--- client/src/pet/PetStat.jsx
+++ client/src/pet/PetStat.jsx
@@ -8,6 +8,7 @@ import {
   CartesianGrid,
   ResponsiveContainer,
 } from 'recharts';
+import { PawPrint } from 'lucide-react';
 
 const PetStat = ({ data }) => {
   return (
@@ -16,20 +17,22 @@ const PetStat = ({ data }) => {
       <div className='border border-neutral-300 shadow-lg shadow-black/10 rounded-xl'>
         <div className='flex justify-between mx-3 my-2'>
             <span className='text-orange-500 font-semibold text-sm'>Total</span>
-            <span className='text-orange-500 font-semibold text-sm'>59 XP</span>
+            <div className='flex items-center gap-1'>
+                <span className='text-orange-500 font-semibold text-sm'>59</span>
+                <PawPrint alt='Paw' className='w-4 h-4 text-orange-500'/>
+            </div>
         </div>
         <ResponsiveContainer width="100%" height={200}>
             <LineChart data={data}>
             <CartesianGrid strokeDasharray="3 3" />
             <XAxis dataKey="day" />
             <YAxis allowDecimals={false} />
-            <Tooltip />
             <Line
                 type="linear"
                 dataKey="xp"
                 stroke="#F97316"
                 strokeWidth={2}
-                dot={{ r: 3, fill: '#f97316' }}
+                dot={{ r: 4, fill: '#f97316' }}
             />
             </LineChart>
         </ResponsiveContainer>
