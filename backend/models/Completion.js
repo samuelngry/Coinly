5fca3b3caef87965faf6fff518d95fe9bbd6e5ba
samuelngry <samuelngry00@gmail.com>
2025-07-10 22:23:04 +0800

    feat: add completion model with id, user_id, date, created_at, updated_at items for tracking daily quest completion

---
+++ backend/models/Completion.js
@@ -0,0 +1,29 @@
+const Sequelize = require('sequelize');
+const db = require("../config/db");
+
+const UserQuest = db.define('DailyCompletion', {
+    id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    date: {
+        type: Sequelize.DATEONLY,
+        allowNull: false
+    },
+    created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn('NOW')
+    },
+    updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn('NOW')
+    }
+});
