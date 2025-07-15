8b80279540ff4a0d026e963cf0d239d9f1e7dd61
samuelngry <samuelngry00@gmail.com>
2025-07-15 15:29:29 +0800

    fix: include token into fetch in leaderboard api

--- client/src/components/leaderboard/Leaderboard.jsx
+++ client/src/components/leaderboard/Leaderboard.jsx
@@ -4,14 +4,27 @@ import { useEffect } from 'react';
 
 const Leaderboard = () => {
     const [leaderboard, setLeaderboard] = useState([]);
-
+    
     useEffect(() => {
-        const fetchLeaderboard = async () => {
-            const res = await fetch("http://localhost:3000/api/leaderboard");
-            const data = await res.json();
-            setLeaderboard(data);
-        };
-        fetchLeaderboard();
+    const fetchLeaderboard = async () => {
+        const token = localStorage.getItem("token");
+
+        const res = await fetch("http://localhost:3000/api/leaderboard", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`
+            }
+        });
+        
+        if (!res.ok) {
+            throw new Error("Failed to fetch leaderboard");
+        }
+        
+        const data = await res.json();
+        setLeaderboard(data);
+    };
+    fetchLeaderboard();
     }, []);
 
   return (
