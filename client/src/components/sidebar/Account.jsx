28f4577407713da6ddf4390a93375f2315597455
samuelngry <samuelngry00@gmail.com>
2025-07-16 23:32:03 +0800

    feat: update sidebar account component to display username and profile picture of user

--- client/src/components/sidebar/Account.jsx
+++ client/src/components/sidebar/Account.jsx
@@ -1,15 +1,16 @@
-const Account = () => {
+import defaultIcon from '../../assets/default.png'
+
+const Account = ({ username, avatarUrl }) => {
   return (
-    <div className="border-b mb-4 pb-4 mt-2 border-stone-300 mr-4">
-      <button className="flex p-0.5 hover:bg-stone-200 rounded transition-colors relative gap-2 w-full items-center">
+    <div className="border-b mb-4 pb-4 mt-2 border-neutral-300 mr-4">
+      <button className="flex p-0.5 hover:bg-neutral-300 rounded transition-colors relative gap-2 w-full items-center">
         <img
-            src="https://api.dicebear.com/9.x/notionists/svg"
+            src={`http://localhost:3000${avatarUrl}` || defaultIcon}
             alt="Avatar"
-            className="size-8 rounded shrink-0 bg-violet-500 shadow"
+            className="w-10 h-10 rounded-full shrink-0"
         />
         <div className="text-start">
-            <span className="text-sm font-semibold block">yii.jing</span>
-            <span className="text-xs text-neutral-500 block">yijing@gmail.com</span>
+            <span className="text-sm font-semibold block">{username}</span>
         </div>
       </button>
     </div>
--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -3,12 +3,46 @@ import Account from './Account'
 import Search from './Search'
 import RouteSelect from './RouteSelect'
 import SidebarFooter from './SidebarFooter'
+import { useEffect, useState } from 'react'
 
 const Sidebar = () => {
+  const [username, setUsername] = useState('');
+  const [avatarUrl, setAvatarUrl] = useState('');
+
+  const getUserData = async () => {
+    try {
+      const token = localStorage.getItem("token");
+
+      const res = await fetch("http://localhost:3000/api/users", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`
+        }
+      });
+
+      if (!res.ok) {
+        throw new Error('Failed to fetch user data');
+      }
+
+      const data = await res.json();
+
+      setUsername(data.username);
+      setAvatarUrl(data.avatar_url);
+
+    } catch (err) {
+      console.error("Failed to fetch user data:", err);
+    }
+  };
+
+  useEffect(() => {
+    getUserData();
+  }, []);
+
   return (
     <div>
         <div className='fixed left-4 top-4 h-screen w-56 flex flex-col'>
-            <Account />
+            <Account username={username} avatarUrl={avatarUrl} />
             <Search />
             <RouteSelect />
             <SidebarFooter />
