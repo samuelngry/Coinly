9a5fccc5becb35602ba3f40a177de0f8a1954981
samuelngry <samuelngry00@gmail.com>
2025-07-16 15:39:01 +0800

    feat: update order of leaderboard to include level and not just xp and increase limit to 20

--- backend/controllers/leaderboardController.js
+++ backend/controllers/leaderboardController.js
@@ -1,18 +1,24 @@
 const User = require("../models/User");
 const Pets = require("../models/Pets");
 
-
 const getLeaderboard = async (req, res) => {
     try {
+        User.hasOne(Pets, { foreignKey: 'user_id', as: 'Pet' });
+        Pets.belongsTo(User, { foreignKey: 'user_id' });
+
         const users = await User.findAll({
             include: {
                 model: Pets,
                 as: 'Pet',
-                attributes: ['mood', 'level', 'xp']
+                attributes: ['mood', 'level', 'xp'],
+                required: true
             },
-            order: [['xp', 'DESC']],
-            limit: 10,
-            attributes: ['id', 'username', 'avatar_url', 'streak_count']
+            attributes: ['id', 'username', 'avatar_url', 'streak_count'],
+            order: [
+                [{ model: Pets, as: 'Pet' }, 'level', 'DESC'],
+                [{ model: Pets, as: 'Pet' }, 'xp', 'DESC']
+            ],
+            limit: 20,
         });
 
         const leaderboard = users.map((user, index) => ({
