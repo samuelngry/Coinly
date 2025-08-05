263a6c0b0733cc19dcaaf6c4cb5b3bf2aead06cc
samuelngry <samuelngry00@gmail.com>
2025-08-05 17:39:48 +0800

    feat: add vite api base url to client

--- backend/server.js
+++ backend/server.js
@@ -8,20 +8,13 @@ const app = express();
 
 const allowedOrigins = [
   'https://coinly-kappa.vercel.app',
-  'http://localhost:5173',        
-  'http://localhost:3000'            
+  'http://localhost:5173',
+  'http://localhost:3000'
 ];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    } else {
-      return callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
+  origin: allowedOrigins,
+  credentials: true
 }));
 
 app.use(express.json());
